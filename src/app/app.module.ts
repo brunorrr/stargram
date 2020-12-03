f49c7646d073d52ffe59cf97cffe38c853a52c7c
Bruno Ricci <brunoriccidossantos@hotmail.com>
2020-12-02 21:55:43 -0300

    Creating main structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { LateralNavComponent } from './components/lateral-nav/lateral-nav.component';
+import { PostComponent } from './components/feed/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavBarComponent,
+    FeedComponent,
+    LateralNavComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/lateral-nav/lateral-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lateral-nav',
+  templateUrl: './lateral-nav.component.html',
+  styleUrls: ['./lateral-nav.component.scss']
+})
+export class LateralNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
