c52634027e5763daaeba330005748aea47b97a53
Bruno Ricci <brunoriccidossantos@hotmail.com>
2020-12-05 02:17:05 -0300

    feat(mock): complete development of mock's first version
    add suport to mock for standalone tests, create an intelligent system to find which mock fits the
    request

--- mock/index.js
+++ mock/index.js
@@ -19,21 +19,49 @@
         }
     });
 
-    mockGroups.forEach( group => {
+    Object.keys(mockGroups).forEach( groupName => {
+        const group = mockGroups[groupName];
         app.all(group[0].path, (req, res, next) => {
             const selectedMock = group.filter(mock => {
-                if(mock.query != undefined) {
-                    if(!mock.query.every(
+                if(mock.method !== req.method) {
+                    return false;
+                }
+                if(mock.query !== undefined) {
+                    if(!Object.keys(mock.query).every(
                         query => req.query[query]
+                    )){
+                        return false;
+                    }
+                }
+                if(mock.body !== undefined) {
+                    if(!Object.keys(mock.body).every(
+                        body => req.body[body]
+                    )){
+                        return false;
+                    }
+                }
+                if(mock.headers !== undefined) {
+                    if(!Object.keys(mock.headers).every(
+                        header => req.headers[header]
                     )){
                         return false;
                     }
                 }
                 return true;
             })[0];
-            if(selectedMock != null ) {
-                res.send(mock.response.body);
+            if(selectedMock !== undefined ) {
+                console.log(`Called mock via ${selectedMock.method} ${selectedMock.path} with response status ${selectedMock.response.status}`);
+                res.status(selectedMock.response.status !== undefined ? selectedMock.response.status : 200)
+                    .send(selectedMock.response.body !== undefined ? selectedMock.response.body : null)
+                    .end(); 
+            } else {
+                res.sendStatus(404);
             }
+            next();
         });
+    });
+
+    app.listen(port, () => {
+        console.log(`Mock server started at port ${port}`);
     });
 })();
