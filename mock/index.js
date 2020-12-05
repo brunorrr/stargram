3666aa146d85336cb7f7a2c0c636352c937cb4bf
Bruno Ricci <brunoriccidossantos@hotmail.com>
2020-12-05 02:50:08 -0300

    refactor(mock): add mock start trigger in the app

--- mock/index.js
+++ mock/index.js
@@ -21,34 +21,41 @@
 
     Object.keys(mockGroups).forEach( groupName => {
         const group = mockGroups[groupName];
+        // For each group of mocks, create a listener for client call, it answer all methods
         app.all(group[0].path, (req, res, next) => {
+            // For each group of mocks, find which mock fits the request, there must be at least one eligible option
             const selectedMock = group.filter(mock => {
+                // Checks if the candidate mock has the same method as the request
                 if(mock.method !== req.method) {
                     return false;
                 }
+                // Checks if all candidate mock queries are equal to the request
                 if(mock.query !== undefined) {
                     if(!Object.keys(mock.query).every(
-                        query => req.query[query]
+                        query => req.query[query] !== undefined
                     )){
                         return false;
                     }
                 }
+                // Checks if the candidate mock body is the same as the request body
                 if(mock.body !== undefined) {
                     if(!Object.keys(mock.body).every(
-                        body => req.body[body]
+                        body => req.body[body] !== undefined
                     )){
                         return false;
                     }
                 }
+                // Checks if all candidate mock headers are equal to the request
                 if(mock.headers !== undefined) {
                     if(!Object.keys(mock.headers).every(
-                        header => req.headers[header]
+                        header => req.headers[header] !== undefined
                     )){
                         return false;
                     }
                 }
                 return true;
             })[0];
+            // If there is at least one eligible mock, select this one and use it as a return
             if(selectedMock !== undefined ) {
                 console.log(`Called mock via ${selectedMock.method} ${selectedMock.path} with response status ${selectedMock.response.status}`);
                 res.status(selectedMock.response.status !== undefined ? selectedMock.response.status : 200)
